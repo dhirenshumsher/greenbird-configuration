a3b2e688bbbf1d88d32da4eb5df1ec7569210d62
magnho <magnus.holm@greenbird.com>
2015-08-24 10:14:19 +0200

    #26: ConfigurationReporter's bean-report no longer throws ConfigurationException

--- src/main/java/com/greenbird/configuration/report/ConfigurationReporter.java
+++ src/main/java/com/greenbird/configuration/report/ConfigurationReporter.java
@@ -1,27 +1,18 @@
 package com.greenbird.configuration.report;
 
-import static ch.lambdaj.Lambda.join;
-import static com.greenbird.configuration.util.SpringContextUtils.getBeanIfAvailable;
-import static java.util.Arrays.asList;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
+import com.google.common.base.Joiner;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Ordering;
+import com.greenbird.configuration.context.SpringContextLoader;
+import com.greenbird.configuration.properties.ConfigurationPropertyPlaceholderConfigurer;
+import com.greenbird.configuration.util.ResourceFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryUtils;
-import org.springframework.beans.factory.HierarchicalBeanFactory;
-import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -29,20 +20,22 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ListMultimap;
-import com.google.common.collect.Ordering;
-import com.greenbird.configuration.ConfigurationException;
-import com.greenbird.configuration.context.SpringContextLoader;
-import com.greenbird.configuration.properties.ConfigurationPropertyPlaceholderConfigurer;
-import com.greenbird.configuration.util.ResourceFinder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static ch.lambdaj.Lambda.join;
+import static com.greenbird.configuration.util.SpringContextUtils.getBeanIfAvailable;
+import static java.util.Arrays.asList;
 
 @Service
 public class ConfigurationReporter implements ApplicationContextAware {
+    public static final String UNKNOWN_PACKAGE = "<unknown package>";
+    public static final String UNKNOWN_CLASS = "<unknown class>";
     private static final String LS = System.getProperty("line.separator");
     private static final String RULER = "***********************************************************************";
-    private static Joiner commaJoiner = Joiner.on(", ");
+    private static final Joiner commaJoiner = Joiner.on(", ");
 
     private Logger logger = LoggerFactory.getLogger(getClass());
     private ApplicationContext applicationContext = null;
@@ -157,7 +150,7 @@ public class ConfigurationReporter implements ApplicationContextAware {
                 .append("BEANS IN CONTEXT")
                 .append(LS)
                 .append("----------------");
-        
+
         Map<String, BeanPackage> packageMap = createPackageMap(((AbstractApplicationContext) applicationContext).getBeanFactory());
         for (String packageName : packageMap.keySet()) {
             packageMap.get(packageName).report(reportBuilder);
@@ -165,143 +158,62 @@ public class ConfigurationReporter implements ApplicationContextAware {
         reportBuilder.append(LS);
     }
 
-	protected static Map<String, BeanPackage> createPackageMap(BeanFactory beanFactory) { 
-		Map<String, BeanPackage> packageMap = new TreeMap<String, BeanPackage>();
-        
-        Map<String, BeanDefinition> beanNamesForTypeIncludingAncestors = beanDefinitonsForTypeIncludingAncestors(beanFactory, Object.class);
-        
+    private static Map<String, BeanPackage> createPackageMap(ConfigurableListableBeanFactory beanFactory) {
+        Map<String, BeanPackage> packageMap = new TreeMap<String, BeanPackage>();
+
+        Map<String, BeanDefinition> beanNamesForTypeIncludingAncestors = beanDefinitionsForTypeIncludingAncestors(beanFactory);
+
         for (Map.Entry<String, BeanDefinition> entry : beanNamesForTypeIncludingAncestors.entrySet()) {
-        	BeanDefinition beanDefinition = entry.getValue();
-
-        	String beanName = entry.getKey();        
-
-        	String className = beanDefinition.getBeanClassName();
-        	try {
-	        	if(className == null) {
-	        		String factoryBeanName = beanDefinition.getFactoryBeanName();
-	        		String factoryMethodName = beanDefinition.getFactoryMethodName();
-	
-	        		if(factoryBeanName != null && factoryMethodName != null) {
-	        			
-	        			BeanDefinition factoryBeanDefinition = beanNamesForTypeIncludingAncestors.get(factoryBeanName);
-	                	if(filterClassName(factoryBeanDefinition.getBeanClassName())) {
-	                        continue;
-	                    }
-	            		className = lookupBeanClassNameFromFactory(beanDefinition, factoryBeanDefinition);
-	        		} else {
-	    				throw new ConfigurationException("Unable to report on bean '" + beanName + "': " + beanDefinition);
-	        		}
-	        	}
-
-        		className = getEnhancedBaseClass(className);
-			} catch(ClassNotFoundException e) {
-				throw new ConfigurationException("Unable to report on bean '" + beanName + "': " + beanDefinition, e);
-			}
-        	if(filterClassName(className)) {
-                continue;
+            BeanDefinition beanDefinition = entry.getValue();
+            String beanName = entry.getKey();
+            String simpleName;
+            String packageName;
+            try {
+                String className = beanDefinition.getBeanClassName();
+                if (className == null) {
+                    String factoryBeanName = beanDefinition.getFactoryBeanName();
+                    BeanDefinition factoryBeanDefinition = beanNamesForTypeIncludingAncestors.get(factoryBeanName);
+                    className = lookupBeanClassNameFromFactory(beanDefinition, factoryBeanDefinition);
+                }
+                if (filterClassName(className)) {
+                    continue;
+                }
+                simpleName = className.substring(className.lastIndexOf('.') + 1);
+                packageName = className.substring(0, className.length() - simpleName.length() - 1);
+            } catch (Exception e) {
+                simpleName = UNKNOWN_CLASS;
+                packageName = UNKNOWN_PACKAGE;
             }
 
-        	String simpleName = className.substring(className.lastIndexOf('.') + 1);
-        	String packageName = className.substring(0, className.length() - simpleName.length() - 1);
-        	        	
             if (!packageMap.containsKey(packageName)) {
                 packageMap.put(packageName, new BeanPackage(packageName));
             }
             packageMap.get(packageName).addBeanClass(beanName, simpleName);
         }
-		return packageMap;
-	}
+        return packageMap;
+    }
 
-	protected static String lookupBeanClassNameFromFactory(BeanDefinition beanDefinition, BeanDefinition factoryBeanDefinition) throws ClassNotFoundException {
+    private static String lookupBeanClassNameFromFactory(BeanDefinition beanDefinition, BeanDefinition factoryBeanDefinition) throws ClassNotFoundException, NoSuchMethodException {
+        Class<?> factoryDefinitionBeanClass = Class.forName(factoryBeanDefinition.getBeanClassName());
+        return factoryDefinitionBeanClass.getMethod(beanDefinition.getFactoryMethodName()).getReturnType().getName();
+    }
 
-		// get the class
-		Class<?> factoryDefinitionBeanClass = Class.forName(getEnhancedBaseClass(factoryBeanDefinition.getBeanClassName()));
-		
-		// get the method return type
-		Method method = null;
-		try {
-			// attempt getting factroy method with no arguments
-			method = factoryDefinitionBeanClass.getMethod(beanDefinition.getFactoryMethodName());
-		} catch(NoSuchMethodException e) {
-			// determine which method matches, and ensure there is only one class with that name
-			Method[] methods = factoryDefinitionBeanClass.getDeclaredMethods();
-			
-			for(Method candiateMethod : methods) {
-				if(candiateMethod.getName().equals(beanDefinition.getFactoryMethodName())) {
-					if(method == null) {
-						method = candiateMethod;
-					} else if(!method.getReturnType().getName().equals(candiateMethod.getReturnType().getName())) {
-						throw new ConfigurationException("Unable to determine bean class for bean: " + beanDefinition, e);
-					}
-				}
-			}
-		}
-		if(method == null) {
-			throw new ConfigurationException("Unable to report locate class " + factoryBeanDefinition.getBeanClassName() + " method " + beanDefinition.getFactoryMethodName());
-		}
-		return getEnhancedBaseClass(method.getReturnType().getName());
-			
-	}
+    private static boolean filterClassName(String className) {
+        return className.startsWith("org.springframework");
+    }
 
-	private static String getEnhancedBaseClass(String className) throws ClassNotFoundException {
-		if(!className.contains("CGLIB")) {
-			return className;
-		}
-		
-   		return getEnhancedBaseClass(Class.forName(className)).getName();
-	}
-
-	private static Class<?> getEnhancedBaseClass(Class<?> beanClass) {
-		if(Enhancer.isEnhanced(beanClass)) {
-			beanClass = beanClass.getSuperclass();
-		}
-
-		return beanClass;
-	}
-
-	
-	private static boolean filterClassName(String className) {
-		return className.startsWith("org.springframework");
-	}
-
-	
-    /**
-     * 
-     * Based of the {@linkplain BeanFactoryUtils#beanNamesForTypeIncludingAncestors(ListableBeanFactory, Class, boolean, boolean)} method
-     * 
-     */
-
-	protected static Map<String, BeanDefinition> beanDefinitonsForTypeIncludingAncestors(BeanFactory factory, Class<Object> type) {
-
-    	Map<String, BeanDefinition> map = new HashMap<String, BeanDefinition>();
-		while (true) {
-			
-			if(factory instanceof ConfigurableListableBeanFactory) {
-				ConfigurableListableBeanFactory lbf = (ConfigurableListableBeanFactory)factory;
-				
-				String[] beanNames = lbf.getBeanNamesForType(type);
-				
-				for(String beanName : beanNames) {
-					if(!map.containsKey(beanName)) {
-						if(lbf.containsBeanDefinition(beanName)) {
-							map.put(beanName, lbf.getBeanDefinition(beanName));
-						}
-					}
-				}
-			}
-
-			if(factory instanceof HierarchicalBeanFactory) {
-				factory = ((HierarchicalBeanFactory)factory).getParentBeanFactory();
-				
-				if(factory instanceof ListableBeanFactory) {
-					continue;
-				}
-			}
-			break;
-		}
-		
-		return map;
-	}
+    private static Map<String, BeanDefinition> beanDefinitionsForTypeIncludingAncestors(ConfigurableListableBeanFactory lbf) {
+        Map<String, BeanDefinition> map = new HashMap<String, BeanDefinition>();
+        String[] beanNames = lbf.getBeanNamesForType(Object.class);
+        for (String beanName : beanNames) {
+            if (!map.containsKey(beanName)) {
+                if (lbf.containsBeanDefinition(beanName)) {
+                    map.put(beanName, lbf.getBeanDefinition(beanName));
+                }
+            }
+        }
+        return map;
+    }
 
     private void buildFooter(StringBuilder reportBuilder) {
         reportBuilder.append(RULER).append(LS).append(LS);
--- src/test/java/com/greenbird/configuration/report/ConfigurationReporterTest.java
+++ src/test/java/com/greenbird/configuration/report/ConfigurationReporterTest.java
@@ -1,24 +1,14 @@
 package com.greenbird.configuration.report;
 
-import com.greenbird.configuration.ConfigurationException;
 import com.greenbird.configuration.ContextLoadingTestBase;
 import com.greenbird.test.logging.TestLogAppender;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.internal.matchers.Any;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.HierarchicalBeanFactory;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.containsString;
@@ -43,7 +33,7 @@ public class ConfigurationReporterTest extends ContextLoadingTestBase {
     public void setApplicationContext_allSubsystemsActive_reportIsLoggedAndContainsExpectedElements() {
         createContextForProfiles("prod", "testprofile");
         String normalizedMessage = getNormalizedMessageFromLogEvent();
-        System.out.println(normalizedMessage);
+
         assertThat(normalizedMessage, containsString("GREENBIRD CONFIGURATION REPORT"));
         assertThat(normalizedMessage, containsString("SPRING PROFILES"));
         assertThat(normalizedMessage, containsString("Active profiles: prod, testprofile Default profiles: default"));
@@ -86,6 +76,7 @@ public class ConfigurationReporterTest extends ContextLoadingTestBase {
     public void setApplicationContext_onlyReportAndContextSubsystemsActive_reportIsLoggedAndContainsExpectedElements() {
         createContextForConfiguration("test-spring-context-report-only.xml", "test-spring-context-contexts-only.xml");
         String normalizedMessage = getNormalizedMessageFromLogEvent();
+
         assertThat(normalizedMessage, containsString("GREENBIRD CONFIGURATION REPORT"));
         assertThat(normalizedMessage, containsString("SPRING PROFILES"));
         assertThat(normalizedMessage, containsString("Active profiles: <none>"));
@@ -136,6 +127,16 @@ public class ConfigurationReporterTest extends ContextLoadingTestBase {
         assertThat(loggingEvents.size(), is(0));
     }
 
+    @Test
+    public void setApplicationContext_missingBeanClassName_loggedUnderUnknownPackage() {
+        createContextForConfiguration("test-spring-context-bean-class-missing.xml");
+        String normalizedMessage = getNormalizedMessageFromLogEvent();
+
+        assertThat(normalizedMessage, containsString("<unknown package>"));
+        assertThat(normalizedMessage, containsString("<unknown class>"));
+        assertThat(normalizedMessage, containsString("(child.bean)"));
+    }
+
     private String getNormalizedMessageFromLogEvent() {
         List<LoggingEvent> loggingEvents = testLogAppender.getLoggingEvents();
         assertThat(loggingEvents.size(), is(1));
@@ -143,162 +144,4 @@ public class ConfigurationReporterTest extends ContextLoadingTestBase {
         assertThat(loggingEvent.getLevel(), is(Level.INFO));
         return loggingEvent.getMessage().toString().replace(LS, " ").replaceAll(" +", " ");
     }
-    
-    @Test
-    public void testBeanDefinitionClass_onFactory_noBeanName() throws ClassNotFoundException {
-    	BeanDefinition beanDefinition = Mockito.mock(BeanDefinition.class);
-    	
-    	Mockito.when(beanDefinition.getFactoryBeanName()).thenReturn(null);
-    	Mockito.when(beanDefinition.getFactoryMethodName()).thenReturn("myMethod");
-
-    	BeanDefinition factoryBeanDefinition = Mockito.mock(BeanDefinition.class);
-    	Mockito.when(factoryBeanDefinition.getBeanClassName()).thenReturn(Object.class.getName());
-
-    	try {
-    		ConfigurationReporter.lookupBeanClassNameFromFactory(beanDefinition, factoryBeanDefinition);
-    		
-    		Assert.fail();
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    }
-    
-    @Test
-    public void testBeanDefinitionClass_onFactory_multipleFactoryMethodsSameReturnType() throws ClassNotFoundException {
-    	BeanDefinition beanDefinition = Mockito.mock(BeanDefinition.class);
-    	
-    	Mockito.when(beanDefinition.getFactoryBeanName()).thenReturn(null);
-    	Mockito.when(beanDefinition.getFactoryMethodName()).thenReturn("contentEquals");
-
-    	BeanDefinition factoryBeanDefinition = Mockito.mock(BeanDefinition.class);
-    	Mockito.when(factoryBeanDefinition.getBeanClassName()).thenReturn(String.class.getName());
-
-   		ConfigurationReporter.lookupBeanClassNameFromFactory(beanDefinition, factoryBeanDefinition);
-    }
-
-    @Test
-    public void testBeanDefinitionClass_onFactory_multipleFactoryMethodsDifferentReturnType() throws ClassNotFoundException {
-    	BeanDefinition beanDefinition = Mockito.mock(BeanDefinition.class);
-    	
-    	Mockito.when(beanDefinition.getFactoryBeanName()).thenReturn(null);
-    	Mockito.when(beanDefinition.getFactoryMethodName()).thenReturn("getEnhancedBaseClass");
-
-    	BeanDefinition factoryBeanDefinition = Mockito.mock(BeanDefinition.class);
-    	Mockito.when(factoryBeanDefinition.getBeanClassName()).thenReturn(ConfigurationReporter.class.getName());
-
-    	try {
-    		ConfigurationReporter.lookupBeanClassNameFromFactory(beanDefinition, factoryBeanDefinition);
-    		
-    		Assert.fail();
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-   		
-    }
-
-    @Test
-    public void testBeanDefinitionClass_onFactory_noProperFactoryBean() {
-    	try {
-    		ConfigurationReporter.createPackageMap(createMockBeanFactory(null, Object.class.getName(), null));
-    		
-    		Assert.fail();
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    	try {
-    		ConfigurationReporter.createPackageMap(createMockBeanFactory(null, null, "toString"));
-    		
-    		Assert.fail();
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    }
-    
-    @Test
-    public void testBeanDefinitionClass_onFactory_unknownClassesBean() {
-    	try {
-    		ConfigurationReporter.createPackageMap(createMockBeanFactory("no.known.class$$CGLIB", null, null));
-    		
-    		Assert.fail();
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    	try {
-    		ConfigurationReporter.createPackageMap(createMockBeanFactory(null, "no.known.class", "toString"));
-    		
-    		Assert.fail();
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    }
-    
-    @Test
-    public void testBeanDefinitionClass_onFactory_springClassesBean() {
-    	try {
-    		Map<String, ?> map = ConfigurationReporter.createPackageMap(createMockBeanFactory(org.springframework.beans.BeanInfoFactory.class.getName(), null, null));
-    		
-    		Assert.assertTrue(map.isEmpty());
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    	try {
-    		Map<String, ?> map = ConfigurationReporter.createPackageMap(createMockBeanFactory(null, org.springframework.beans.BeanInfoFactory.class.getName(), "toString"));
-    		
-    		Assert.assertTrue(map.isEmpty());
-    	} catch(ConfigurationException e) {
-    		// pass
-    	}
-    }
-
-    @Test
-    public void testBeanDefinitions_emptyFactory() {
-    	BeanFactory factory = Mockito.mock(BeanFactory.class);
-    	
-    	Map<String, BeanDefinition> beanDefinitonsForTypeIncludingAncestors = ConfigurationReporter.beanDefinitonsForTypeIncludingAncestors(factory, Object.class);
-    	Assert.assertEquals(0, beanDefinitonsForTypeIncludingAncestors.size());
-    }
-    
-    @Test
-    public void testBeanDefinitions_factory() {
-    	BeanFactory factory = createMockBeanFactory(null, org.springframework.beans.BeanInfoFactory.class.getName(), "toString");
-    	
-    	Map<String, BeanDefinition> beanDefinitonsForTypeIncludingAncestors = ConfigurationReporter.beanDefinitonsForTypeIncludingAncestors(factory, Object.class);
-    	Assert.assertEquals(2, beanDefinitonsForTypeIncludingAncestors.size());
-    }
-
-    @Test
-    public void testBeanDefinitions_simpleParentFactory() {
-    	HierarchicalBeanFactory factory = Mockito.mock(HierarchicalBeanFactory.class);
-    	BeanFactory parentFactory = Mockito.mock(BeanFactory.class);
-    	
-    	Mockito.when(factory.getParentBeanFactory()).thenReturn(parentFactory);
-    	
-    	Map<String, BeanDefinition> beanDefinitonsForTypeIncludingAncestors = ConfigurationReporter.beanDefinitonsForTypeIncludingAncestors(factory, Object.class);
-    	Assert.assertEquals(0, beanDefinitonsForTypeIncludingAncestors.size());
-
-    	Mockito.verify(factory, Mockito.atLeastOnce()).getParentBeanFactory();
-    }
-    
-    private BeanFactory createMockBeanFactory(String beanClassName, String factoryBeanClassName, String factoryMethodName) {
-    	BeanDefinition beanDefinition = Mockito.mock(BeanDefinition.class);
-    	Mockito.when(beanDefinition.getBeanClassName()).thenReturn(beanClassName);
-    	Mockito.when(beanDefinition.getFactoryBeanName()).thenReturn("myFactoryBean");
-    	Mockito.when(beanDefinition.getFactoryMethodName()).thenReturn(factoryMethodName);
-    	
-    	BeanDefinition factoryBeanDefinition = Mockito.mock(BeanDefinition.class);
-    	Mockito.when(factoryBeanDefinition.getBeanClassName()).thenReturn(factoryBeanClassName);
-    	
-    	ConfigurableListableBeanFactory factory = Mockito.mock(ConfigurableListableBeanFactory.class);
-    	BeanFactory parentFactory = Mockito.mock(BeanFactory.class);
-    	
-    	Mockito.when(factory.getParentBeanFactory()).thenReturn(parentFactory);
-    	Mockito.when(factory.getBeanNamesForType(Object.class)).thenReturn(new String[]{"myBean", "myFactoryBean"});
-    	
-    	Mockito.when(factory.getBeanDefinition("myFactoryBean")).thenReturn(factoryBeanDefinition);
-    	Mockito.when(factory.getBeanDefinition("myBean")).thenReturn(beanDefinition);
-    	Mockito.when(factory.containsBeanDefinition(Mockito.anyString())).thenReturn(Boolean.TRUE);
-    	
-    	return factory;
-    }
-
 }
